d183a24ea92412ac45a2c8bcd1dba6ba7e920bc6
Timothy Gibson <ryaninlux@gmail.com>
2022-05-22 12:36:42 -0700

    Add initial files and environment

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
-      title: 'template', //Change!!!!!!!!
+      title: 'weather', 
       template: './src/index.html',
       inject: 'body'
     })
